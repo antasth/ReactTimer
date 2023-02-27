760bf3274bba1d8ec66003c2151fc42169db89d1
antasth <antasth@mail.ru>
2023-02-27 22:51:12 +0300

    fix styles in ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,20 +7,26 @@ const ProductCard = ({ brand, model, imageUrl, price, avalible }) => {
       <div className={styles.image}>
         <img src={imageUrl[0]} alt="product_img"></img>
       </div>
+
       <div className={styles.description}>
         <div className={styles.title}>
           <div className={styles.brand}>{brand}</div>
           <div className={styles.model}>{model}</div>
         </div>
-        <div className={styles.price}>{price} ₽.</div>
-        <div className={styles.credit}>От {Math.round(price / 18)} ₽./мес.</div>
-        <div className={styles.stock}>
-          {avalible ? 'В наличии' : 'Нет в наличии'}
-        </div>
-        <div className={styles.button}>
-          <Button type="default" className={styles.buy_button}>
-            {avalible ? 'Купить' : 'Заказать'}
-          </Button>
+
+        <div className={styles.info}>
+          <div className={styles.price}>{price} $.</div>
+          <div className={styles.credit}>
+            От {Math.round(price / 18)} $./мес.
+          </div>
+          <div className={styles.stock}>
+            {avalible ? 'В наличии' : 'Нет в наличии'}
+          </div>
+          <div className={styles.button}>
+            <Button type="default" className={styles.buy_button}>
+              {avalible ? 'Купить' : 'Заказать'}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
@@ -28,3 +34,4 @@ const ProductCard = ({ brand, model, imageUrl, price, avalible }) => {
 }
 
 export { ProductCard }
+
