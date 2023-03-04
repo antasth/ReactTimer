1726658df17ff883630fd4d2e9bdb3f0add95247
antasth <antasth@mail.ru>
2023-03-04 23:18:02 +0300

    add functions in Search component

--- src/components/UI/Search/Search.jsx
+++ src/components/UI/Search/Search.jsx
@@ -1,17 +1,28 @@
+import { useContext } from 'react'
 import { FaSearch } from 'react-icons/fa'
+import { SortContext } from '../../../context/SortContext'
 import styles from './Search.module.scss'
 
 const Search = () => {
+  const sort = useContext(SortContext)
+
   return (
     <div className={styles.search_box}>
-      <button className={styles.btn_search}>
-        <FaSearch className={styles.icon}/>
-      </button>
-      <input
-        type="text"
-        className={styles.input_search}
-        placeholder="Поиск по каталогу..."
-      />
+      <form>
+        <button
+          className={styles.btn_search}
+          onClick={(e) => e.preventDefault()}
+        >
+          <FaSearch className={styles.icon} />
+        </button>
+        <input
+          type="text"
+          className={styles.input_search}
+          value={sort.filter}
+          placeholder="Поиск по каталогу..."
+          onChange={(e) => sort.setFilterParams(e.target.value)}
+        />
+      </form>
     </div>
   )
 }
