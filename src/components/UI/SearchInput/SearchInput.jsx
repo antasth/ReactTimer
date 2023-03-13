7d302606db72490fb7c80dc25e30b27d74d83d21
antasth <antasth@mail.ru>
2023-03-13 18:05:54 +0300

    refactor: change styles in SearchInput component (Mon, Mar 13, 2023  6:05:54 PM)

--- src/components/UI/SearchInput/SearchInput.jsx
+++ src/components/UI/SearchInput/SearchInput.jsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
-import {FaSearch} from 'react-icons/fa';
+import { BsSearch } from 'react-icons/bs';
+import { VscClose } from 'react-icons/vsc';
 import { SortContext } from '../../../context/SortContext';
-import styles from './SearchInput.module.scss'
+import styles from './SearchInput.module.scss';
 
 const SearchInput = () => {
   const {search, setSearchParams} = useContext(SortContext)
@@ -9,12 +10,13 @@ const SearchInput = () => {
   return (
     <div className={styles.search_box}>
       <form className={styles.search_form}>
+{/* 
         <button
           className={styles.btn_search}
           onClick={(e) => e.preventDefault()}
         >
-          <FaSearch className={styles.icon} />
-        </button>
+        </button> */}
+        <BsSearch id='icon'/>
         <input
           type="text"
           className={styles.input_search}
@@ -22,9 +24,12 @@ const SearchInput = () => {
           placeholder="Поиск..."
           onChange={(e) => setSearchParams(e.target.value)}
         />
+        <VscClose className={styles.close_icon} id='icon'/>
+
       </form>
     </div>
   )
 }
 
-export { SearchInput }
+export { SearchInput };
+
