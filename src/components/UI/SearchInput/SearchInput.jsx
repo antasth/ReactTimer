20649275fd3eccc1acf676c48553d8fcac4b5ad7
antasth <antasth@mail.ru>
2023-03-14 20:36:50 +0300

    feat: clear search params when click on clear icon and preventdefault on submit form (Tue, Mar 14, 2023  8:36:50 PM)

--- src/components/UI/SearchInput/SearchInput.jsx
+++ src/components/UI/SearchInput/SearchInput.jsx
@@ -1,32 +1,33 @@
-import { useContext } from 'react';
-import { CgSearch } from 'react-icons/cg';
-import { VscClose } from 'react-icons/vsc';
-import { SortContext } from '../../../context/SortContext';
-import styles from './SearchInput.module.scss';
+import { useContext } from 'react'
+import { CgSearch } from 'react-icons/cg'
+import { VscClose } from 'react-icons/vsc'
+import { SortContext } from '../../../context/SortContext'
+import styles from './SearchInput.module.scss'
 
 const SearchInput = () => {
-  const {search, setSearchParams} = useContext(SortContext)
+  const { search, setSearchParams } = useContext(SortContext)
 
   return (
     <div className={styles.search_box}>
-      <form className={styles.search_form}>
+      <form className={styles.search_form} onSubmit={(e) => e.preventDefault()}>
         <div className={styles.form_content}>
-
-        <CgSearch id='icon'/>
-        <input
-          type="text"
-          className={styles.input_search}
-          value={search}
-          placeholder="Поиск..."
-          onChange={(e) => setSearchParams(e.target.value)}
-        />
-        <VscClose className={styles.close_icon} id='icon'/>
+          <CgSearch id="icon" />
+          <input
+            type="text"
+            className={styles.input_search}
+            value={search}
+            placeholder="Поиск..."
+            onChange={(e) => setSearchParams(e.target.value)}
+          />
+          <VscClose
+            className={styles.close_icon}
+            id="icon"
+            onClick={() => setSearchParams('')}
+          />
         </div>
-
       </form>
     </div>
   )
 }
 
-export { SearchInput };
-
+export { SearchInput }
